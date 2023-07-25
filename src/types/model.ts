4eb270f6bf957dc5e6bbdb4d7ba64cfedca3b9ba
Tricogimmick <nomura@tricogimmick.com>
2023-07-25 17:48:39 +0900

    year ページの追加

---
+++ src/types/model.ts
@@ -0,0 +1,46 @@
+export type MagazineType = {
+	id: string,
+	title: string,
+	publisher: string|null,
+	description: string|null
+}
+
+export type IssueType = {
+	id: string,
+	magazine_id: string,
+	title: string,
+	cover_url: string | null,
+	toc_url: string | null,
+	description: string | null
+}
+
+export type ContentType = {
+    issue_id: string,
+    order_no: number,
+    title_id: number,
+    title: string,
+    page_no: number,
+    content_type: number,
+    color: number | null,
+    serialization_status: number | null,
+    description: string | null,
+    aurthors: string | null,
+    scripters: string | null
+}
+
+export type TitleType = {
+    id: number,
+    title: string,
+    content_type: number,
+    publication: string,
+    completion: number,
+    description: string
+}
+
+export type AuthorType = {
+    id: number,
+    name: string,
+    kana: string,
+    author_type: string,
+    description: string
+}
