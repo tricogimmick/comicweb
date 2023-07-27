349f00f638048cbc7d7508a3036379016cfda6ed
Tricogimmick <nomura@tricogimmick.com>
2023-07-27 13:07:50 +0900

    Github Pages へのデプロイ設定を

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://tricogimmick.github.io',
     base: '/comic_magazine_data'
 });
