8ff1e585127392dfac75b14122a5fa19a133eff3
Tricogimmick <nomura@tricogimmick.com>
2023-07-27 17:20:46 +0900

    リンクを絶対PATHに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tricogimmick.github.io',
-    base: '/comic_magazine_data',
-    outDir: "./docs"
+    base: '/comic_magazine_data'
 });
