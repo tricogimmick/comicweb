1429e6dc24eac71bcfdab220d5723b63d0f31f70
Tricogimmick <nomura@tricogimmick.com>
2023-07-27 15:55:48 +0900

    ビルド設定変更と微修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tricogimmick.github.io',
-    base: '/comic_magazine_data'
+    base: '/comic_magazine_data',
+    outDir: "./docs"
 });
