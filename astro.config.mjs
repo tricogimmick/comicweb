b99945694541c372a27b316defb9e0c0ceb02f1b
Tricogimmick <nomura@tricogimmick.com>
2023-11-01 10:56:04 +0900

    不要な雑誌の非表示対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tricogimmick.github.io',
-    base: '/comic_magazine_data'
-});
+    base: '/comic_magazine_data',
+    build: {
+        inlineStylesheets: 'always'
+      }
+  });
+
+  
